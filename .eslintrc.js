0a07452a7aa3cc944152481ffabec3083d8a2a61
Confiks <confiks@scriptbase.org>
2018-07-03 16:59:52 +0200

    First set of eslint rules beyond the defaults. Specific React/JSX rules still to modify.

---
+++ .eslintrc.js
@@ -0,0 +1,111 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {'__DEV__': true},
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+        jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    // Handpicked from list at: https://eslint.org/docs/rules/
+
+    // Possible errors
+    'no-template-curly-in-string': 'error',
+
+    // Best practices
+    'array-callback-return': 'error',
+    'consistent-return': 'error',
+    'curly': ['error', 'multi-or-nest', 'consistent'],
+    'eqeqeq': 'error',
+    'no-alert': 'error',
+    'no-caller': 'error',
+    'no-else-return': 'error',
+    'no-eval': 'error',
+    'no-extend-native': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-coercion': 'error',
+    'no-implied-eval': 'error',
+    'no-invalid-this': 'error',
+    'no-labels': 'error',
+    'no-lone-blocks': 'error',
+    'no-multi-spaces': 'warn',
+    'no-new': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    'no-octal-escape': 'error',
+    'no-param-reassign': 'warn',
+    'no-proto': 'error',
+    'no-return-assign': 'error',
+    'no-script-url': 'error',
+    'no-self-compare': 'error',
+    'no-sequences': 'error',
+    'no-throw-literal': 'error',
+    'no-unused-expressions': 'error',
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-return': 'error',
+    'no-void': 'error',
+    'no-with': 'error',
+    'prefer-promise-reject-errors': 'warn',
+    'radix': 'error',
+    'require-await': 'warn',
+
+    // Variables
+    'no-shadow': 'warn',
+    'no-shadow-restricted-names': 'error',
+    'no-undef-init': 'error',
+
+    // Stylistic
+    'array-bracket-spacing': ['warn', 'never'],
+    'block-spacing': ['warn', 'always'],
+    'brace-style': ['error', '1tbs'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'comma-spacing': ['error'],
+    'func-call-spacing': ['error', 'never'],
+    // 'indent': ['error', 2, {SwitchCase: 1}],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'key-spacing': 'error',
+    'keyword-spacing': 'error',
+    'line-comment-position': ['warn', {position: 'above'}],
+    'linebreak-style': ['error', 'unix'],
+    'new-parens': 'error',
+    'no-array-constructor': 'error',
+    'no-bitwise': 'warn',
+    'no-mixed-operators': 'warn',
+    'no-multi-assign': 'warn',
+    'no-multiple-empty-lines': 'warn',
+    'no-new-object': 'error',
+    'no-trailing-spaces': 'warn',
+    'quote-props': ['warn', 'consistent-as-needed'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'semi-spacing': 'error',
+    'semi-style': 'error',
+    'space-before-blocks': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
+    'spaced-comment': ['error', 'always'],
+    'unicode-bom': 'error',
+
+    // ECMAScript 6
+    'arrow-spacing': 'error',
+    'no-confusing-arrow': 'warn',
+    'no-duplicate-imports': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn',
+    'prefer-template': 'warn',
+    'rest-spread-spacing': 'error',
+    'template-curly-spacing': 'warn',
+  }
+}
